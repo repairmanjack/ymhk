f2918782a3f092eaa00c96563a70a8e2d6d25d87
Дмитрий Батищев <lxtr@rambler.ru>
2022-09-03 16:48:29 +0300

    запоминаем статус повтора одного трека

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let restoreRepeatStateHandlerExists = false
+
 for (const [method, selector] of Object.entries({
     'play': '.player-controls__btn_play',
     'pause': '.player-controls__btn_pause',
@@ -5,6 +7,17 @@ for (const [method, selector] of Object.entries({
     'nexttrack': '.player-controls__btn_next'
 })) {
     navigator.mediaSession.setActionHandler(method, () => {
+        let repeatElement = document.querySelector('.player-controls__btn_repeat'),
+            repeatOne = repeatElement && repeatElement.classList.contains('player-controls__btn_repeat_state2');
+
         document.querySelector(selector).dispatchEvent(new Event('click'));
+
+        if (repeatOne && !restoreRepeatStateHandlerExists && ['nexttrack', 'previoustrack'].includes(method)) {
+            restoreRepeatStateHandlerExists = true;
+            document.querySelector('.progress__line').addEventListener('transitionstart', () => {
+                restoreRepeatStateHandlerExists = false;
+                repeatElement.dispatchEvent(new Event('click'));
+            }, {once: true});
+        }
     });
 }
